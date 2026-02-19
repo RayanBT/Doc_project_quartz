72837cd04d8859bc171deb13db32dd6f66da456e
Rayan BEN TANFOUS <btrayan21@gmail.com>
2026-02-19 22:34:53 +0100

    suppression fichier

--- page_quartz/content/labs/td5/scripts/tofu/live/ci-cd-permissions/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "lambda_deploy_plan_role_arn" {
-  description = "The ARN of the IAM role for plan"
-  value       = module.iam_roles.lambda_deploy_plan_role_arn
-}
-
-output "lambda_deploy_apply_role_arn" {
-  description = "The ARN of the IAM role for apply"
-  value       = module.iam_roles.lambda_deploy_apply_role_arn
-}
--- page_quartz/content/labs/td5/scripts/tofu/live/lambda-sample/backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "devops-lab-tofu-state"
-    key            = "td5/scripts/tofu/live/lambda-sample/backend.tf"
-    region         = "us-east-2"
-    encrypt        = true
-    dynamodb_table = "devops-lab-tofu-state"
-  }
-}
